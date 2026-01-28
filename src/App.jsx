3840de4219e70e5f7f522fc2dc00aca9db72f846
ZSzabolcs <zelenaksz@kkszki.hu>
2026-01-28 08:27:55 +0100

    Nagyon sok izé

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +10,17 @@ function App() {
   return (
     <>
       <h1>Teszt</h1>
-      <nav>
-        <NavLink></NavLink>
+      <nav className='d-flex gap-4 p-3 fs-3'>
+        <NavLink to="/listaOsszes"></NavLink>
+        <NavLink to="/listaEgy"></NavLink>
+        <NavLink to="/modositas"></NavLink>
+        <NavLink to="/feltoltes"></NavLink>
+        <NavLink to="/torles"></NavLink>
       </nav>
+
+      <Routes>
+        <Route path='/lista' element={}></Route>
+      </Routes>
     </>
   )
 }
