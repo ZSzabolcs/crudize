7f4335069991dcd2a4f3408a7179e87121c1d2e5
ZSzabolcs <zelenaksz@kkszki.hu>
2026-01-28 10:02:51 +0100

    Nem tom mit csináltam, valami van.

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -6,4 +6,22 @@ function List(){
     )
 }
 
+function ListaKomponens({elemek}){
+    return(
+        <>
+            {elemek.map((elem,index) => (
+                <>
+                    <div key={index}>
+                        Név: {elem.name} Elem: {elem.email}
+                        <button><Link to={"/listaEgy/" + elem.id} className="btn btn-primary"><i className="bi bi-eye"></i>Kilistázza a kiválasztott cuccot</Link></button>
+                        <button><Link to={"/modositas/" + elem.id} className="btn btn-primary"><i className="bi bi-eye"></i>Mit írjak ide ember</Link></button>
+                        <button><Link to={"/torles/" + elem.id} className="btn btn-primary"><i className="bi bi-eye"></i>Mit írjak ide ember</Link></button>
+
+                    </div>
+                </>
+            ))}
+        </>
+    )
+}
+
 export default List;
